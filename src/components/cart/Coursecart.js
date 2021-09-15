6b2206c4073b569410074064d1705a26c83b5f1a
pi996-code <puspendraupraity00@gmail.com>
2021-09-15 19:05:16 +0530

    add data item url in coursecart

--- src/components/cart/Coursecart.js
+++ src/components/cart/Coursecart.js
@@ -34,7 +34,7 @@ export default class Coursecart extends Component {
                     data-item-id={node.id}
                     data-item-name={node.title}
                     data-item-price={node.price}
-                    data-item-url="https://learncodeonline.in"
+                    data-item-url="https://learnecom.netlify.app/"
                     data-item-image={node.image.fixed.src}                    
                     className="btn btn-warning snipcart-add-item">Join now</button>
                   </div>
