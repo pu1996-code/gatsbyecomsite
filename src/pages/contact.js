a7d4013e4fbbaa800ff39276a83e2b74cf614d82
pi996-code <puspendraupraity00@gmail.com>
2021-09-15 16:33:14 +0530

    Added dummy contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,38 @@
+import * as React from "react"
+import { Link } from "gatsby"
+// import { StaticImage } from "gatsby-plugin-image"
+import {  graphql } from "gatsby"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import HeroSection from "../components/Reuseable/HeroSection";
+import InfoBlock from "../components/Reuseable/InfoBlock";
+import DualInfoBlock from "../components/Reuseable/DualInfoBlock";
+import Teamphotosection from "../components/about/Teamphotosection";
+
+const AboutPage = ({data}) => (
+  <Layout>
+    <Seo title="Home" />
+    <HeroSection
+    img={data.img.childImageSharp.fluid}
+    title="About learnCodeOnline"
+    subtitle=""
+    heroclass="about-background"
+    />
+    <DualInfoBlock heading='A Message From CEO'/>
+    <InfoBlock heading="About Vision"></InfoBlock>
+    <Teamphotosection></Teamphotosection>
+  </Layout>
+)
+
+export const quesry = graphql `
+{
+  img: file(relativePath: {eq: "about.png"}) {
+    childImageSharp {
+      fluid(maxWidth: 400, maxHeight: 250) {
+        ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+}
+`
+export default AboutPage
