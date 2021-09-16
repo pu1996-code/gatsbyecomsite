fe81a93fd656575e1c3641f3623d121c61c783e4
pi996-code <puspendraupraity00@gmail.com>
2021-09-16 14:27:23 +0530

    created category

--- src/components/cart/Coursecart.js
+++ src/components/cart/Coursecart.js
@@ -2,12 +2,39 @@ import React, { Component } from "react"
 import Heading from "../Reuseable/Heading"
 import Img from "gatsby-image"
 
+
+const getCaty = item =>{
+  let holdItems = item.map(item =>{
+    return item.node.category
+  })
+
+  let holdCategories = new Set(holdItems)
+  let categories = Array.from(holdCategories)
+  categories = ["All",...categories]
+  return categories
+}
+
 export default class Coursecart extends Component {
   constructor(props) {
     super(props)
     this.state = {
       courses: props.courses.edges,
       mycourses: props.courses.edges,
+      mycategories:getCaty(props.courses.edges)
+    }
+  }
+  catyClicked = category=>{
+    let keepItsafe = [...this.state.courses]
+    if(category === "All"){
+      this.setState(()=>{
+        return {mycourses:keepItsafe}
+      })
+    }
+    else{
+      let  holdme = keepItsafe.filter(({node})=>node.category === category)
+      this.setState(()=>{
+        return {mycourses:holdme}
+      }) 
     }
   }
   render() {
@@ -16,6 +43,26 @@ export default class Coursecart extends Component {
       <section className="py-5 ">
         <div className="container">
           <Heading title="courses" />
+          <div className="row my-3">
+            <div className="col-10 mx-auto text-center">
+              {
+                this.state.mycategories.map((category,index)=>{
+                  return(
+                    <button
+                    className="btn btn-info m-3 px-3"
+                    key={index}
+                    onClick={()=>{
+                      this.catyClicked(category)
+                    }}
+                    >
+                      {category}
+
+                    </button>
+                  )
+                })
+              }
+            </div>
+          </div>
           <div className="row">
             {this.state.mycourses.map(({node}) => {
               return (
